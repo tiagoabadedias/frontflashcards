2fa313f8d8bdf150be236fc279536bf98b560c50
Tiago <tiagoabadedias@hotmail.com>
2026-02-07 10:38:26 -0300

    first commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // @ts-ignore
 export const api = axios.create({
   // @ts-ignore
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || 'https://flashcards-sooty-ten.vercel.app/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
